9dec82938094fb86aa3d241d34a23850c6bc6437
aazizk <khan.abdulaziz@gmail.com>
2022-03-29 17:33:19 +0500

    Update exercise.py

--- lang/lists/exercise.py
+++ lang/lists/exercise.py
@@ -3,11 +3,14 @@ strings = []
 names = ["John", "Eric", "Jessica"]
 
 # write your code here
-second_name = None
+second_name = names[1]
+for i in range(3):
+    numbers.append(i)
+for j in ["hello", "world"]:
+    strings.append(j)
 
 
 # this code should write out the filled arrays and the second name in the names list (Eric).
 print(numbers)
 print(strings)
 print("The second name on the names list is %s" % second_name)
-
