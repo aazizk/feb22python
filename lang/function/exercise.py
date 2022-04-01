e8e1bbb522df059b771e7ccce3b08f8c4f859546
aazizk <khan.abdulaziz@gmail.com>
2022-04-01 10:14:05 +0500

    Update exercise.py

--- lang/function/exercise.py
+++ lang/function/exercise.py
@@ -1,10 +1,12 @@
 # Modify this function to return a list of strings as defined above
 def list_benefits():
-    return("More organized code", "More readable code", "Easier code reuse", "Allowing programmers to share and connect code together")
+    benefits = ("More organized code", "More readable code", "Easier code reuse", "Allowing programmers to share and connect code together")
+    return(benefits)
 
 # Modify this function to concatenate to each benefit - " is a benefit of functions!"
 def build_sentence(benefit):
-    return( benefit + " is a benefit of functions!")
+    benefit = benefit + " is a benefit of functions!"
+    return(benefit)
 
 def name_the_benefits_of_functions():
     list_of_benefits = list_benefits()
